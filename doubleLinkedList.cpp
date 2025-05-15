2f083b1c966b9f4e71ac2b20bc0b8e5082cd7937
riswandaP <riswandaputra03@gmail.com>
2025-05-15 11:09:21 +0700

    Step 1: Telusuri daftar untuk menemukan simpulnya

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -94,4 +94,14 @@ public:
 
         Node *current = START;
 
+        // Step 1: Traverse the list to find the node
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
+
 };
