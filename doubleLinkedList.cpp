9fba0283a24d8a726ccc77c089640715d3563c3b
riswandaP <riswandaputra03@gmail.com>
2025-05-15 10:30:58 +0700

    Step 1

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -27,6 +27,11 @@ class DoubleLinkedList
         string nm;
         cout << "\nEnter the roll number of student";
         cin >> nim;
+
+        // Step 1: Alocate memory for new node
+        Node *newNode = new Node();
+
+       
     }
 
 }
