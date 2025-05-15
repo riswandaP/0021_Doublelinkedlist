b7211c6310166c4a931094adce61acff1f19157f
riswandaP <riswandaputra03@gmail.com>
2025-05-15 11:17:14 +0700

    Step 2: Jika node adalah awalan

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -104,4 +104,34 @@ public:
             return;
         }
 
+        // Step 2: If node is at the beginning
+        if (current == START)
+        {
+            START = current->next;
+            if (START != NULL)
+                START->next = NULL;
+        }
+        else
+        {
+            // Step 3: Link previous node to next of current
+            current->prev->next = current->next;
+
+            // Step 4: If current is not the last node
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+        }
+
+        // Step 5: Deleted the node
+        delete current;
+        cout << "Record with roll number "  << rollNo << " deleted" << endl;
+    }
+
+    void traverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+        }
+    }
+
 };
