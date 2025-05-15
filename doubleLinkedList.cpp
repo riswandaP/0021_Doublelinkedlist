fedbda2d9de78954b43047ff03b31b9ff4e35dcb
riswandaP <riswandaputra03@gmail.com>
2025-05-15 11:03:14 +0700

    Step 8

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -57,5 +57,19 @@ public:
             return;
         }
 
+        // Insert in between node
+        // Step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll number not allowed" << endl;
+            return;
+        }
+
 
 };
